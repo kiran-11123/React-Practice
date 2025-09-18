31220aab4864e9ffc3dfbd187bcaafa14baa85a6
Kiran <kiran.sadara6730@gmail.com>
2025-09-18 18:53:34 +0530

    learning  re-rendering

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,48 +3,55 @@ import './App.css'
 
 function App() {
 
+
+
     return(
-     <div>
-      
-      
-      <Counter/>
+     <div >
 
-     
-
-
-
-
+        <PostComponent/>
      </div>
 
     )
   }
 
+  const style= {backgroundColor:"white" , borderRaduis:10 , borderColor:"gray" , borderWidth:1 ,display:"flex" ,gap:4}
+
+  function PostComponent(){ 
+
+    return(
+
+      <div >
 
   
-  function Counter(){
+        <div style={style}>
 
 
-      const[count,setCount] = useState(0);
-   
-    useEffect(()=>{
+          <img src ="vite.svg" alt="image"  style={{ width:20 , height:20 ,borderRadius:20}} />
+          
 
+          <div style={{  fontSize:15 , marginLeft:5}}>
+              <b>100x devs</b>
 
-      setInterval(()=>{
-      setCount(10000);
-    
-    },1000)
+              <div>23,0000 followers</div>
 
-    console.log('mounted')
+              <div>12m</div>
+          </div>
 
-    },[])
-    
-    console.log('count');
+        
 
-  return  <p>Count :{count}</p>
+          </div>
 
+            <div>
+               Want to know how to win big ? check out how the things work
+          </div>
+
+    </div>
+
+    )
+     
   }
 
 
- 
+  
 
 export default App
