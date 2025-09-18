062d40b78e008938c19957f9e1bcab8dd0961f90
Kiran <kiran.sadara6730@gmail.com>
2025-09-18 19:03:49 +0530

    learning conditional-rendering

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,54 +3,24 @@ import './App.css'
 
 function App() {
 
+  const [visible , setVisible] = useState(false);
+
+  function toggle(){
+       setVisible(!visible);
+  }
+
 
 
     return(
      <div >
 
-        <PostComponent/>
+       <button onClick={toggle}>Click me</button>
+
+       {visible && <div> This is visible </div>}
      </div>
 
     )
   }
-
-  const style= {backgroundColor:"white" , borderRaduis:10 , borderColor:"gray" , borderWidth:1 ,display:"flex" ,gap:4}
-
-  function PostComponent(){ 
-
-    return(
-
-      <div >
-
-  
-        <div style={style}>
-
-
-          <img src ="vite.svg" alt="image"  style={{ width:20 , height:20 ,borderRadius:20}} />
-          
-
-          <div style={{  fontSize:15 , marginLeft:5}}>
-              <b>100x devs</b>
-
-              <div>23,0000 followers</div>
-
-              <div>12m</div>
-          </div>
-
-        
-
-          </div>
-
-            <div>
-               Want to know how to win big ? check out how the things work
-          </div>
-
-    </div>
-
-    )
-     
-  }
-
 
   
 
