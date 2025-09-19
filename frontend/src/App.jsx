22ce47b07f48b85ce41871f6cf1510d115cd11a0
Kiran <kiran.sadara6730@gmail.com>
2025-09-19 10:04:51 +0530

    Links in react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,34 @@
 import { useState ,useEffect} from 'react'
 import './App.css'
-import { BrowserRouter , Routes , Route } from 'react-router-dom'
+import { BrowserRouter , Routes , Route ,Link } from 'react-router-dom'
 import Nav from './components/Nav'
 import Home from './components/Home'
+import Projects from './components/Projects'
 function App() {
       
      
   return(
-      <BrowserRouter>
+
+    <div>
+
+   
+        <BrowserRouter>
+
+         <Link to="/">Nav</Link>  | <Link to="/home">Home</Link>  | <Link to="/projects">Projects</Link>
 
       <Routes>
 
       <Route path="/" element={<Nav/>} />
       <Route path="/home" element={<Home/>} />
+      <Route path="/projects" element={<Projects/>} />
 
       </Routes>
 
       </BrowserRouter>
+
+
+
+      </div>
   )
 
   
