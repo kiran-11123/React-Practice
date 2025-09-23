9ed758c5a3f8581aa295e23e16f0ce714de7fc04
Kiran <kiran.sadara6730@gmail.com>
2025-09-23 19:55:36 +0530

    toggling modes by doing changes in the tailwind.config.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,21 @@ import './App.css'
 import './index.css'
 import SidebarClass1 from './answers/1.basic-project'
 import Sidebar2Transistion from './sidebars/Sidebar2Transistions'
+import { useSearchParams } from 'react-router-dom'
+import { useState } from 'react'
 
 function App() {
+
+
   return(
    
-    <div>
-
-       <SidebarClass1/>
+    <div className='h-screen bg-white dark:bg-black'>
+          
+      <h1 className='text-black dark:text-white'>hi there</h1>
+      
+      <button onClick={()=>{
+         document.querySelector("html").classList.toggle("dark")
+      }}  className='dark:text-white ' >Toggle Theme</button>
 
 
 
