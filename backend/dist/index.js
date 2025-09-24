8e774f609700e54a2bbba560ef0898181a187099
Kiran <kiran.sadara6730@gmail.com>
2025-09-24 19:29:13 +0530

    types in typescript

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,4 +1,8 @@
 "use strict";
-let greet = () => {
-    console.log("Hello World");
-};
+function sum(a, b) {
+    const A = typeof (a) === 'string' ? parseFloat(a) : a;
+    const B = typeof (b) === 'string' ? parseFloat(b) : b;
+    return A + B;
+}
+let res = sum(10, 20);
+console.log(res);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,12 @@
-let greet = ()=>{
-    console.log("Hello World");
+type sumInput = string | number;
+
+function sum(a:sumInput , b:sumInput){
+      
+    const A = typeof(a)==='string' ? parseFloat(a):a;
+    const B = typeof(b)==='string' ? parseFloat(b):b;
+
+    return A+B;
 }
+
+let res:number = sum(10,20);
+console.log(res);
