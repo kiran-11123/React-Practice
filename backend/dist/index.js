20028500f21240d3716b66081f000e3c4d9a02bc
Kiran <kiran.sadara6730@gmail.com>
2025-09-28 17:05:19 +0530

    Nested Interfaces

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,11 +1,21 @@
 "use strict";
-//interfaces
 const input = {
     emp_name: "Kiran",
-    department: "SCB"
+    department: "SCB",
+    age: 24,
+    address: {
+        city: "bengaluru",
+        country: "India",
+        pincode: 251100
+    }
 };
-function greet(name) {
-    return `Hello ${name.emp_name} , department ${name.department}`;
+function greet(details) {
+    if (details.age > 18 && details.address.country === 'India') {
+        return `${details.emp_name} is Eligible to Vote in ${details.address.city}`;
+    }
+    else {
+        return `${details.emp_name} is not Eligible to Vote in ${details.address.city}`;
+    }
 }
 const result = greet(input);
 console.log(result);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,41 @@
 //interfaces
+interface Address{
+    city:String;
+    country:String,
+    pincode :number
+}
+
 
 interface Employee {
    emp_name:String;
    department:String;
+   age:number;
+   address:Address
 
 
 }
 
 const input = {
  emp_name : "Kiran",
- department :"SCB"
+ department :"SCB",
+ age:24,
+ address:{
+    city:"bengaluru",
+    country:"India",
+    pincode :251100
+ }
 }
 
 
 
-function greet(name:Employee):String{
-     return `Hello ${name.emp_name} , department ${name.department}`
+function greet(details:Employee):String{
+     
+    if(details.age >18 && details.address.country==='India'){
+        return `${details.emp_name} is Eligible to Vote in ${details.address.city}`;
+    }
+    else{
+        return `${details.emp_name} is not Eligible to Vote in ${details.address.city}`
+    }
 }
 
 const result:String = greet(input);
