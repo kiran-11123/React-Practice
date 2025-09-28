454bbc292d1832ee3742da7ea28b236e249a6504
Kiran <kiran.sadara6730@gmail.com>
2025-09-28 19:34:16 +0530

    functions with types

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,16 +1,14 @@
 "use strict";
-//types have unions and intersections
-const user = {
-    name: "Kiran",
-    age: 23,
-};
-function User(user) {
-    if ('name' in user && 'age' in user) {
-        // Inside here, TypeScript knows it's a User – safe to access!
-        return `Student name is ${user.name} and age is ${user.age}`;
+function greet(user) {
+    if ('name' in user && 'permissions' in user) {
+        return `Admin name is : ${user.name} and permissions is ${user.permissions}`;
     }
-    // Fallback if it's just a Student
-    return `This is a student with grade ${user.grade}`;
+    else {
+        return `User name is : ${user.name} and age is ${user.age}`;
+    }
 }
-const x = User(user);
+const x = greet({
+    name: "Kiran",
+    age: 25
+});
 console.log(x);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,38 +1,28 @@
-//types have unions and intersections
-
-type User = {
+interface Admin{
     name:string;
-    age:number;
-}
-type Student ={
-    grade:string
+    permissions:string;
 }
 
-type CollegeStudent = User | Student;
+interface User{
+    name:string;
+    age:number
+}
 
+function greet(user : Admin | User):string{
 
+   if('name' in user && 'permissions' in user){
+     return `Admin name is : ${user.name} and permissions is ${user.permissions}`
+   }
+   else{
+      return `User name is : ${user.name } and age is ${user.age}`
+   }
 
-const user ={
+}
 
+const x = greet({
     name:"Kiran",
-    age:23,
-    
-   
-
-}
-
-function User(user:CollegeStudent):string{
-      if ('name' in user && 'age' in user) {
-        // Inside here, TypeScript knows it's a User – safe to access!
-        return `Student name is ${user.name} and age is ${user.age}`;
-    }
-    // Fallback if it's just a Student
-    return `This is a student with grade ${(user as Student).grade}`;
-
-}
-
-const x = User(user);
+    age:25
+})
 
 console.log(x);
-
 
