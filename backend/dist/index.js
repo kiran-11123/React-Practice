ff182dca51dd18ec9905dfa35f24c67ecf5ba445
Kiran <kiran.sadara6730@gmail.com>
2025-09-28 17:15:04 +0530

    function in the interface

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,20 +1,9 @@
 "use strict";
-const input = {
-    emp_name: "Kiran",
+const Person = {
+    name: "Kiran",
     age: 24,
-    address: {
-        city: "bengaluru",
-        country: "India",
-        pincode: 251100
+    greet() {
+        return `Hi ${name}`;
     }
 };
-function greet(details) {
-    if (details.age > 18 && details.address.country === 'India') {
-        return `${details.emp_name} is Eligible to Vote in ${details.address.city}`;
-    }
-    else {
-        return `${details.emp_name} is not Eligible to Vote in ${details.address.city}`;
-    }
-}
-const result = greet(input);
-console.log(result);
+console.log(Person);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,41 +1,17 @@
-//interfaces
-interface Address{
-    city:String;
-    country:String,
-    pincode :number
+interface People{
+    name :string;
+    age:number;
+    greet:()=> string;
+
+    greet2?():string;
 }
 
-
-interface Employee {
-   emp_name:String | number;
-   department?:String;
-   age:number;
-   address:Address
-
-
+const Person : People ={
+      name:"Kiran",
+      age:24,
+      greet(){
+         return `Hi ${name}`
+      }
 }
 
-const input = {
- emp_name : "Kiran",
- age:24,
- address:{
-    city:"bengaluru",
-    country:"India",
-    pincode :251100
- }
-}
-
-
-
-function greet(details:Employee):String{
-     
-    if(details.age >18 && details.address.country==='India'){
-        return `${details.emp_name} is Eligible to Vote in ${details.address.city}`;
-    }
-    else{
-        return `${details.emp_name} is not Eligible to Vote in ${details.address.city}`
-    }
-}
-
-const result:String = greet(input);
-console.log(result);
+console.log(Person);
