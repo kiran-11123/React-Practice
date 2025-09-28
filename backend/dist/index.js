854c88061051a07d1cc6f1a0e8288618dd851259
Kiran <kiran.sadara6730@gmail.com>
2025-09-28 19:28:44 +0530

    union either all or one : one

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -3,10 +3,14 @@
 const user = {
     name: "Kiran",
     age: 23,
-    grade: "A"
 };
 function User(user) {
-    return `Student name is ${user.name} and Grade is ${user.grade}`;
+    if ('name' in user && 'age' in user) {
+        // Inside here, TypeScript knows it's a User – safe to access!
+        return `Student name is ${user.name} and age is ${user.age}`;
+    }
+    // Fallback if it's just a Student
+    return `This is a student with grade ${user.grade}`;
 }
 const x = User(user);
 console.log(x);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,7 +8,7 @@ type Student ={
     grade:string
 }
 
-type CollegeStudent = User & Student
+type CollegeStudent = User | Student;
 
 
 
@@ -16,14 +16,20 @@ const user ={
 
     name:"Kiran",
     age:23,
-    grade:"A"
+    
+   
 
 }
 
 function User(user:CollegeStudent):string{
-      return `Student name is ${user.name} and Grade is ${user.grade}`
-}
+      if ('name' in user && 'age' in user) {
+        // Inside here, TypeScript knows it's a User – safe to access!
+        return `Student name is ${user.name} and age is ${user.age}`;
+    }
+    // Fallback if it's just a Student
+    return `This is a student with grade ${(user as Student).grade}`;
 
+}
 
 const x = User(user);
 
